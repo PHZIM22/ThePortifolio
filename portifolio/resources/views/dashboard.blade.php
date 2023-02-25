42b561dc9c57f92e80b9a1769518d0c4d497ed18
LuisH <luishovieira@yahoo.com.br>
2023-02-24 22:14:44 -0300

    foi criado o controller About

--- portifolio/resources/views/dashboard.blade.php
+++ portifolio/resources/views/dashboard.blade.php
@@ -1,69 +1,8 @@
 @extends('layouts.layout')
 @section('title', 'Dashbord')
 @section('content')
-<!-- navbar dashboard bootstrap 4.6-->
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
 
-  <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
-          About
-        </a>
-        <ul class="dropdown-menu">
-          <li><a class="dropdown-item" href="#" data-toggle="modal" data-target="#modalAbout">Add</a></li>
-          <li><a class="dropdown-item" href="#">list</a></li>
-         
-        </ul>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#data-toggle="modal" data-target="#modalservice">Add</a></li>"
-          Service
-        </a>
-        <ul class="dropdown-menu">
-          <li><a class="dropdown-item" href="#data-toggle="modal" data-target="#modalportifolio">Add</a></li>"
-          <li><a class="dropdown-item" href="#">list</a></li>
-         
-        </ul>
-        <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
-          Portifolio
-        </a>
-        <ul class="dropdown-menu">
-          <li><a class="dropdown-item" href="#data-toggle="modal" data-target="#modaltestimonials">Add</a></li>"
-          <li><a class="dropdown-item" href="#">list</a></li>
-         
-        </ul>
-        <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
-          Testimonials
-        </a>
-        <ul class="dropdown-menu">
-          <li><a class="dropdown-item" href="#data-toggle="modal" data-target="#modalsignature">Add</a></li>"
-          <li><a class="dropdown-item" href="#">list</a></li>
-         
-        </ul>
-        <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
-          Signature
-        </a>
-        <ul class="dropdown-menu">
-          <li><a class="dropdown-item" href="#">Add</a></li>
-          <li><a class="dropdown-item" href="#">list</a></li>
-         
-        </ul>
-      </li>
-    </ul>
-  </div>
-</nav>
-<!--fim navbar dashboard-->
+<x-dashboard.navbar/>
 @php
     $x ="list";
 @endphp
@@ -73,6 +12,15 @@
 @elseif ($x == "list")
     <x-dashboard.liste/>
 @else
+@endif
+
+@if (isset($msg))
+<div class="alert alert-success" role="alert">
+    {{$msg}}
+</div>
+@else
+
+
 @endif
 
 <x-dashboard.about-modal/>
